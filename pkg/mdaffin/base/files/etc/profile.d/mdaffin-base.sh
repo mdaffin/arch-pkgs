fb5dc7c626897e308fa948371728a5f9bd8ad155
Michael Daffin <michael@daffin.io>
2021-06-03 13:59:24 +0100

    Merges all the packages into one split package

--- pkg/mdaffin/base/files/etc/profile.d/mdaffin-base.sh
+++
@@ -1,5 +0,0 @@
-export EDITOR='nvim'
-export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
-export PATH=$HOME/.local/bin:$PATH
-
-alias ls=/usr/bin/exa
--- pkg/mdaffin/files-base/etc/profile.d/mdaffin-base.sh
+++ pkg/mdaffin/files-base/etc/profile.d/mdaffin-base.sh
@@ -1,3 +1,9 @@
+export EDITOR='nvim'
+export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
+export PATH=$HOME/.local/bin:$PATH
+
+alias ls=/usr/bin/exa
+
 # Node
 export PATH=$HOME/.local/share/npm/bin:$HOME/.yarn/bin:$PATH
 
