3dfedcbc36f210737b285933a17ca5e1e37cc377
Michael Daffin <michael@daffin.io>
2021-06-04 01:20:03 +0100

    Fixes issues with neovim package

--- pkg/mdaffin/files-neovim/etc/profile.d/base.sh
+++
@@ -1,8 +0,0 @@
-PATH=$HOME/.local/share/npm/bin:$HOME/.yarn/bin:$PATH
-PATH=$HOME/.cargo/bin:$PATH
-PATH=$HOME/.local/bin:$PATH
-
-export PATH
-
-export FZF_DEFAULT_COMMAND='rg --files'
-export FZF_DEFAULT_OPTS='-m --height 50% --border'
